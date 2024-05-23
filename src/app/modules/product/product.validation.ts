3b862bfff9af0b8ec1bb72b0005eab4dab26e406
alimbdit <alimbdit@gmail.com>
2024-05-23 06:29:26 +0600

    product delete  added

--- src/app/modules/product/product.validation.ts
+++ src/app/modules/product/product.validation.ts
@@ -3,56 +3,56 @@ import Joi from 'joi';
 // joi validation schema
 
 const variantJoiValidationSchema = Joi.object({
-  type: Joi.string()
-    .trim()
-    .required()
-    .messages({ 'any.required': 'VARIANT TYPE is required' }),
-  value: Joi.string()
-    .trim()
-    .required()
-    .messages({ 'any.required': 'VARIANT VALUE is required' }),
+    type: Joi.string()
+        .trim()
+        .required()
+        .messages({ 'any.required': 'VARIANT TYPE is required' }),
+    value: Joi.string()
+        .trim()
+        .required()
+        .messages({ 'any.required': 'VARIANT VALUE is required' }),
 });
 const inventoryJoiValidationSchema = Joi.object({
-  quantity: Joi.number()
-    .integer()
-    .min(0)
-    .required()
-    .messages({ 'any.required': 'INVENTORY QUANTITY is required' }),
-  inStock: Joi.boolean()
-    .required()
-    .messages({ 'any.required': 'INVENTORY STOCK is required' }),
+    quantity: Joi.number()
+        .integer()
+        .min(0)
+        .required()
+        .messages({ 'any.required': 'INVENTORY QUANTITY is required' }),
+    inStock: Joi.boolean()
+        .required()
+        .messages({ 'any.required': 'INVENTORY STOCK is required' }),
 });
 
 const productJoiValidationSchema = Joi.object({
-  name: Joi.string()
-    .required()
-    .max(50)
-    .messages({ 'any.required': 'PRODUCT NAME is required' }),
-  description: Joi.string()
-    .min(10)
-    .required()
-    .messages({ 'any.required': 'DESCRIPTION is required' }),
-  price: Joi.number()
-    .required()
-    .min(0)
-    .messages({ 'any.required': 'PRICE is required' }),
-  category: Joi.string()
-    .required()
-    .messages({ 'any.required': 'CATEGORY is required' }),
-  tags: Joi.array()
-    .items(Joi.string().trim())
-    .min(1)
-    .required()
-    .messages({ 'any.required': 'TAGS is required' }),
-  variants: Joi.array()
-    .items(variantJoiValidationSchema)
-    .min(1)
-    .required()
-    .messages({ 'any.required': 'VARIANT is required' }),
-  inventory: inventoryJoiValidationSchema
-    .required()
-    .messages({ 'any.required': 'INVENTORY is required' }),
-  isDeleted: Joi.boolean(),
+    name: Joi.string()
+        .required()
+        .max(50)
+        .messages({ 'any.required': 'PRODUCT NAME is required' }),
+    description: Joi.string()
+        .min(10)
+        .required()
+        .messages({ 'any.required': 'DESCRIPTION is required' }),
+    price: Joi.number()
+        .required()
+        .min(0)
+        .messages({ 'any.required': 'PRICE is required' }),
+    category: Joi.string()
+        .required()
+        .messages({ 'any.required': 'CATEGORY is required' }),
+    tags: Joi.array()
+        .items(Joi.string().trim())
+        .min(1)
+        .required()
+        .messages({ 'any.required': 'TAGS is required' }),
+    variants: Joi.array()
+        .items(variantJoiValidationSchema)
+        .min(1)
+        .required()
+        .messages({ 'any.required': 'VARIANT is required' }),
+    inventory: inventoryJoiValidationSchema
+        .required()
+        .messages({ 'any.required': 'INVENTORY is required' }),
+    //   isDeleted: Joi.boolean(),
 });
 
 export default productJoiValidationSchema;
